3eb16107d7cff855901d24f866977c5835c98cf0
Michael Waskosky <2141654+waskosky@users.noreply.github.com>
2023-09-27 12:23:01 -0600

    Add filter to unsubscribe button redirect
    To make it so that users aren't prompted to login just to unusbscribe to an email type

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -4119,7 +4119,8 @@ function bp_email_unsubscribe_handler() {
 	);
 
 	bp_core_add_message( $message , $message_type );
-	bp_core_redirect( bp_core_get_user_domain( $raw_user_id ) );
+	//bp_core_redirect( bp_core_get_user_domain( $raw_user_id ) );
+	bp_core_redirect( apply_filters( 'bp_email_unsubscribe_handler_redirect', bp_core_get_user_domain( $raw_user_id ) ) );
 
 	exit;
 }
